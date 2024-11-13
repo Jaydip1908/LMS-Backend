886087c59e9d868ef72f4e1a0a306f1f8d961077
jaydip khandala <f451khandalajaydip@gmail.com>
2024-11-13 12:09:18 +0530

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/api/user', (req, res) => {
+    res.send("hello world!");
+});
+
+app.listen(8000, () => {
+    console.log('listening on port', 8000);
+});
